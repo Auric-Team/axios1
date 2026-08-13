99f120930e6b148d6ae0322c64d9502d8d0f547e
Auric-Team <auricxofficial@gmail.com>
2026-08-13 10:06:44 +0530

    fix: Use reflection for Shizuku newProcess invocation in Kotlin MainActivity

--- frontend/android/app/src/main/kotlin/com/axios/installer/installer/MainActivity.kt
+++ frontend/android/app/src/main/kotlin/com/axios/installer/installer/MainActivity.kt
@@ -100,7 +100,14 @@ class MainActivity : FlutterActivity() {
                             return@setMethodCallHandler
                         }
 
-                        val process = Shizuku.newProcess(arrayOf("sh", "-c", command), null, null)
+                        val newProcessMethod = Shizuku::class.java.getDeclaredMethod(
+                            "newProcess",
+                            Array<String>::class.java,
+                            Array<String>::class.java,
+                            String::class.java
+                        )
+                        newProcessMethod.isAccessible = true
+                        val process = newProcessMethod.invoke(null, arrayOf("sh", "-c", command), null, null) as Process
                         val reader = BufferedReader(InputStreamReader(process.inputStream))
                         val errReader = BufferedReader(InputStreamReader(process.errorStream))
 
